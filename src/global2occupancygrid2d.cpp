d5cef2acb51dc8fc5d1016731ae306a5f14705bc
Artur Eisele <artur.eisele@rwth-aachen.de>
2024-05-27 15:06:34 +0000

    T

--- src/global2occupancygrid2d.cpp
+++ src/global2occupancygrid2d.cpp
@@ -35,9 +35,11 @@ void Global2OccupancyGrid2D::pub_occupancy_grid_2D_from_globalmap(global_map_car
     occupancy_grid.header.stamp = stamp;
     //occupancy_grid.header.stamp = ros::Time::now();
 
-    for(auto cell:map.occupied_cell_idx_list)
+    for(auto cellDouble:map.occupied_cell_percentage_idx_list)
     {
-        occupancy_grid.data.at(cell(0)+cell(1)*map2d_nx)=100;
+        auto cell = cellDouble.vec
+        double occup3Dprob = cellDouble.value
+        occupancy_grid.data.at(cell(0)+cell(1)*map2d_nx)=std::max(occupancy_grid.data.at(cell(0)+cell(1)*map2d_nx),occup3Dprob);
     }
 
     this->occupancygrid_pub.publish(occupancy_grid);
